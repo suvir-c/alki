6c831e0e02f803598f170d81e40f1a571858b1fe
Amulya Bhattarai <amulsbhat@gmail.com>
2018-04-15 01:08:30 -0700

    Tooltips on click, toggle for hide/show

--- map/dashboard/src/MapContainer.js
+++ map/dashboard/src/MapContainer.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 import SideBar from "./SideBar";
 import "./App.css";
 import collision from './data/collision_data.json';
 import grapefruit from './grapefruit smol.png';
+import acai from './acai smol.png';
 
 export class MapContainer extends Component {
 
@@ -12,11 +13,13 @@ export class MapContainer extends Component {
         let image = grapefruit;
         let collisionsIndex = 0;
         let collisions = collision.filter((item) => {
-            return collisionsIndex < 10 && item.lat !== 0 && item.long !== 0;
+            return item.lat !== 0 && item.long !== 0 && item.alcohol !== "A";
         }).map((item) => {
             collisionsIndex++;
             return (
                 <Marker
+                    onClick = {this.onMarkerClick}
+                    data = {item}
                     icon = {image}
                     key = {collisionsIndex}
                     position={{lat: parseFloat(item.long), lng: parseFloat(item.lat)}}
@@ -24,21 +27,93 @@ export class MapContainer extends Component {
             );
         });
 
+        let duiIndex = 0;
+        image = acai;
+        let duis = collision.filter((item) => {
+            return item.lat !== 0 && item.long !== 0 && item.alcohol !== "-";
+
+        }).map((item => {
+            duiIndex++;
+            return (
+                <Marker
+                    onClick = {this.onMarkerClick}
+                    data = {item}
+                    icon = {image}
+                    key = {duiIndex}
+                    position={{lat: parseFloat(item.long), lng: parseFloat(item.lat)}}
+                />
+            )
+        }));
         this.state = {
-            collisions: collisions
+            collisions: collisions,
+            duis: duis,
+            hideCollisions: false,
+            hideDUIs: false,
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {}
         }
     }
 
-    onMarkerClick = (props, marker, e) => {
+    onDUIToggle = () => {
+        let curDUIs = this.state.hideDUIs;
+        this.setState({
+            hideDUIs: !curDUIs
+        });
+    };
 
-    }
+    onCollisionToggle = () => {
+        let curCollisions = this.state.hideCollisions;
+        this.setState({
+            hideCollisions: !curCollisions
+        });
+    };
+
+    onMarkerClick = (props, marker, e) => {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        })
+    };
+
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    };
 
     render() {
+        let collisions = null;
+        if (!this.state.hideCollisions) {
+            collisions = this.state.collisions;
+        }
+
+        let duis = null;
+        if (!this.state.hideDUIs) {
+            duis = this.state.duis;
+        }
+
+        let selectedPlaceInfo = [];
+        for (var key in this.state.activeMarker.data) {
+            selectedPlaceInfo.push("" + key +": " + this.state.activeMarker.data[key]);
+        }
+
+        let renderSelectedPlace = selectedPlaceInfo.map((item) => {
+            return (
+                <p>{item}</p>
+            )
+        });
+
         return (
             <div className='sideBySide'>
-                <SideBar />
+                <SideBar onDUIToggle = {this.onDUIToggle.bind(this)} onCollisionToggle = {this.onCollisionToggle.bind(this)} />
                 <div className="map">
                     <Map google={this.props.google}
+                         onClick={this.onMapClicked}
                         initialCenter={{
                              lat:34.052235,
                              lng: -118.24683
@@ -48,7 +123,15 @@ export class MapContainer extends Component {
                              height: '97%'
                          }}
                          zoom={12}>
-                        {this.state.collisions}
+                        {collisions}
+                        {duis}
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}>
+                            <div>
+                                {renderSelectedPlace}
+                            </div>
+                        </InfoWindow>
                     </Map>
                 </div>
             </div>
--- map/dashboard/src/SideBar.js
+++ map/dashboard/src/SideBar.js
@@ -8,13 +8,13 @@ class SideBar extends Component {
         return (
             <div className="sidenav">
                 <p className="title">Alki</p>
-                <div>
-                    <img src={grapefruit} alt="grapefruit" className="pull-left"/>
-                    <span className="pull-right" style={{'font-size': '22px'}}> DUI</span>
-                </div>
-                <div>
+                <div onClick={this.props.onDUIToggle} className="dui" style={{'font-size': '22px'}}>
                     <img src={acai} alt="acai" className="pull-left"/>
-                    <span className="pull-right" style={{'font-size': '22px'}}> Collisions</span>
+                    <span className="pull-right" > DUI</span>
+                </div>
+                <div onClick={this.props.onCollisionToggle} className="collisions" style={{'font-size': '22px'}}>
+                    <img src={grapefruit} alt="grapefruit" className="pull-left"/>
+                    <span className="pull-right"> Collisions</span>
                 </div>
             </div>
         );
